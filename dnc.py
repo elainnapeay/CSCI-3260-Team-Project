d6042e6f26526822231020a04db120c8f758babb
elainnapeay <elainnapeay@gmail.com>
2023-05-11 19:06:03 -0400

    added comments

--- dnc.py
+++ dnc.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+#declare arrays A and B
 A = np.array([[1,1,1,1],
     [1,1,1,1],
     [1,1,1,1],
@@ -10,12 +11,12 @@ B = np.array([[1,1,1,1],
     [1,1,1,1],
     [1,1,1,1]])
 
-
+#array for result
 C = np.array([[0,0,0,0],
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0]])
-
+#function to split matrix into four quadrants
 def split(matrix):
 
 	row, col = matrix.shape
@@ -31,20 +32,21 @@ def DivideConquer(A,B):
     if n == 1:
         return A * B  
     else:
+	#Split matrices into quadrants
         a11,a12,a21,a22 = split(A)
 
         b11,b12,b21,b22 = split(B)
 
         c11,c12,c21,c22 = split(C)
-
+	#divide and conquer for MM
         c11 = DivideConquer(a11, b11) + DivideConquer(a12,b21)
         c12 = DivideConquer(a11, b12) + DivideConquer(a11,b22)       
         c21 =  DivideConquer(a21, b11) + DivideConquer(a22,b21)
         c22 =  DivideConquer(a21, b12) + DivideConquer(a22,b22)
-        
+        #combine into result matrix C
         C = np.vstack((np.hstack((c11, c12)), np.hstack((c21, c22))))
         
         return C
         
-
+#print result of MM
 print(DivideConquer(A,B))
